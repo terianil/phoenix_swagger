a94fe034cfc5631410d1be510afa209f5c111a25
Kamil Maciejczek <maciejczek.kamil@gmail.com>
2021-09-27 11:34:02 +0200

    drop Poison from deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,6 @@ defmodule PhoenixSwagger.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:poison, "~> 2.2 or ~> 3.0", optional: true},
       {:jason, "~> 1.0", optional: true},
       {:ex_json_schema, "~> 0.7.1", optional: true},
       {:plug, "~> 1.11"},
